b00773ed4db473f87cccac3b48bc44fc330482f4
Ole Pelser <opelser@f0r4s11.codam.nl>
2023-01-08 22:14:51 +0100

    Working main loop for printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,33 +6,53 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/05 22:04:06 by opelser       #+#    #+#                 */
-/*   Updated: 2023/01/06 17:03:01 by opelser       ########   odam.nl         */
+/*   Updated: 2023/01/08 22:13:54 by opelser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-// parse through the format string to see how many % conversions to make
-// put all the conversions into a variable
-// put all the special \\ characters in a table/function
-// print the variable
-
+#include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
 
 int		ft_printf(const char *format, ...)
 {
-	va_list		ptr;
-	printf("%s\n", format);
+	va_list			ptr;
+	int				i;
+
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			write(1, "[ % ]", 6);
+			i += 2;
+			continue ;
+		}
+		write(1, &format[i], 1);
+		i++;
+	}
 	va_start(ptr, format);
-	printf("%c\n", va_arg(ptr, int));
+
 	va_end(ptr);
 	return (0);
 }
 
 int		main(void)
 {
-	char c = 'A';
-	test("acdef", c);
+	ft_printf("abc%sde\\f");
 	return (0);
 }
+
+/*
+%c: This specifier is used to print a single character.
+%s: This specifier is used to print a string of characters.
+%p: This specifier is used to print a pointer value.
+%d: This specifier is used to print a signed integer in decimal format.
+%i: This specifier is also used to print a signed integer, but in decimal, octal, or hexadecimal format, depending on the value of the integer and the format flags specified.
+%u: This specifier is used to print an unsigned integer in decimal format.
+%x: This specifier is used to print an unsigned integer in hexadecimal format, using lowercase letters for the digits a through f.
+%X: This specifier is used to print an unsigned integer in hexadecimal format, using uppercase letters for the digits A through F.
+%%: This specifier is used to print a percent sign.
+*/
