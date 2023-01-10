70f9549e9601e8a8bb193c6f0a6f5d650c2a94ae
Ole Pelser <opelser@f0r1s9.codam.nl>
2023-01-10 17:55:24 +0100

    Only norm left

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/05 22:04:06 by opelser       #+#    #+#                 */
-/*   Updated: 2023/01/10 16:03:37 by opelser       ########   odam.nl         */
+/*   Updated: 2023/01/10 17:54:47 by opelser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,24 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+char	*ft_strrchr(const char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char) c)
+			return ((char *)(s + i));
+		i++;
+	}
+	return (NULL);
+}
+
 int	jumptable(const char *format, int specifier, va_list va_ptr)
 {
 	int					count;
+	static const char	format_specifiers[10] = "cspdiuxX%";
 	static t_function	function_array[126] = {
 	['c'] = ft_printf_c,
 	['s'] = ft_printf_s,
@@ -30,24 +45,38 @@ int	jumptable(const char *format, int specifier, va_list va_ptr)
 	['%'] = ft_printf_percent,
 	};
 
+	if (!ft_strrchr(format_specifiers, format[specifier]))
+	{
+		if (write(1, "%", 1) == -1)
+			return (-1);
+		if (write(1, &format[specifier], 1) == -1)
+			return (-1);
+		return (2);
+	}
 	count = function_array[(unsigned char) format[specifier]](va_ptr);
 	return (count);
 }
 
-int	ft_printf(const char *format, ...)
+int	ft_printed_length(const char *format, va_list va_ptr)
 {
-	va_list		va_ptr;
-	int			i;
-	int			count;
-	int			tmp;
+	int		i;
+	int		count;
+	int		tmp;
 
-	va_start(va_ptr, format);
-	count = 0;
 	i = 0;
+	count = 0;
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
+			if (format[i + 1] == '\0')
+			{
+				if ((write(1, "%", 1)) == -1)
+					return (-1);
+				i++;
+				count++;
+				return (count);
+			}
 			tmp = jumptable(format, (i + 1), va_ptr);
 			if (tmp < 0)
 				return (-1);
@@ -60,6 +89,16 @@ int	ft_printf(const char *format, ...)
 		count++;
 		i++;
 	}
+	return (count);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list		va_ptr;
+	int			count;
+
+	va_start(va_ptr, format);
+	count = ft_printed_length(format, va_ptr);
 	va_end(va_ptr);
 	return (count);
 }
@@ -67,7 +106,7 @@ int	ft_printf(const char *format, ...)
 // int		main(void)
 // {
 // 	char	c = 'X';
-// 	// char	*s = "XXXX";
+// 	char	*s = "XXXX";
 // 	char	*p = "pointer test";
 // 	int		d = -78901;
 // 	unsigned int u = 198296479;
@@ -75,11 +114,14 @@ int	ft_printf(const char *format, ...)
 
 // 	int		ret;
 
-// 	ret = ft_printf("my printf:\n\nchar: %c\nstr: %s\nptr: %p\n", c, NULL, p);
-// 	ret += ft_printf("dec: %d\nuns: %u\nhex: %x\nhexup: %X\n\t%%", d, u, x, x);
-// 	printf("\n\nreturn: %d\n", ret);
-// 	ret = printf("\n\nprintf:\n\nchar: %c\nstr: %s\nptr: %p\n", c, NULL, p);
-// 	ret += printf("dec: %d\nuns: %u\nhex: %x\nhexup: %X\n\t%%", d, u, x, x);
+// 	ret = ft_printf("my printf:\n\nchar: %c\nstr: %s\nptr: %p\n", c, s, p);
+// 	ret += ft_printf("dec: %d\nuns: %u\nhex: %x\nhexup: %X\n", d, u, x, x);
+// 	ret += ft_printf("percent edge cases: | %%%rabc | %%% | %");
+// 	printf("\n\nreturn: %d\n\n| --------------------------- |", ret);
+
+// 	ret = printf("\n\nprintf:\n\nchar: %c\nstr: %s\nptr: %p\n", c, s, p);
+// 	ret += printf("dec: %d\nuns: %u\nhex: %x\nhexup: %X\n", d, u, x, x);
+// 	ret += printf("percent edge cases: | %%%rabc | %%% | %");
 // 	printf("\n\nreturn: %d\n", ret);
 // 	return (0);
 // }
--- ft_printf/ft_printf_conversions.c
+++ ft_printf/ft_printf_conversions.c
@@ -6,7 +6,7 @@
 /*   By: opelser <opelser@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/08 19:56:28 by opelser       #+#    #+#                 */
-/*   Updated: 2023/01/10 16:36:31 by opelser       ########   odam.nl         */
+/*   Updated: 2023/01/10 16:52:25 by opelser       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ int	ft_conversion(unsigned long num, int base, char *characters)
 
 	org = count;
 	if (num / base != 0)
-		ft_conversion((num / base), base, characters);
+	{
+		if (ft_conversion((num / base), base, characters) == -1)
+			return (-1);
+	}
 	if (write(1, &characters[num % base], 1) == -1)
 		return (-1);
 	else
