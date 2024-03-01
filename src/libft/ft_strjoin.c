2001c467649e328f62343ee1d4fe8a9b31d322e6
olebol <ole.pelser@gmail.com>
2024-03-01 15:45:33 +0100

    Update ft_strjoin function implementation to work if one of the strings is empty

--- src/libft/ft_strjoin.c
+++ src/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: opelser <opelser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 23:33:42 by opelser           #+#    #+#             */
-/*   Updated: 2024/02/22 16:10:10 by opelser          ###   ########.fr       */
+/*   Updated: 2024/03/01 15:38:13 by opelser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,20 @@
 
 char	*ft_strjoin(const char *s1, char const *s2)
 {
-	char	*str;
-	size_t	s1_len;
-	size_t	s2_len;
+	const size_t	s1_len = ft_strlen(s1);
+	const size_t	s2_len = ft_strlen(s2);
+	char			*result;
 
-	if (!s1 || !s2)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	str = malloc((s1_len + s2_len + 1) * sizeof(char));
-	if (!str)
+	result = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!result)
 		return (NULL);
-	ft_strcpy((char *) str, (char *) s1);
-	ft_strcpy((char *) str + s1_len, s2);
-	str[s1_len + s2_len] = '\0';
-	return (str);
+	if (s1)
+		ft_strcpy((char *) result, s1);
+	if (s2)
+		ft_strcpy((char *) result + s1_len, s2);
+	return (result);
 }
 
 char	*ft_strjoin_replace(char *s1, char const *s2)
@@ -36,6 +35,7 @@ char	*ft_strjoin_replace(char *s1, char const *s2)
 	char	*tmp;
 
 	tmp = ft_strjoin(s1, s2);
-	free(s1);
+	if (s1)
+		free(s1);
 	return (tmp);
 }
